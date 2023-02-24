2c426c5b221166de886c512c17c4b00a2a867104
GiovannaSXX <giovanna.silva548@etec.sp.gov.br>
2023-02-24 17:52:46 -0300

    subindo app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+
+
+<a href="{{ url('/contato') }}" >contato</a>
+<a href="{{ url('/produtos') }}" >produtos</a>
+<a href="{{ url('/empresa') }}">empresa</a>
+<br>
+<br>
+<br>
+Oie, aqui é a home
+</body>
+</html>
